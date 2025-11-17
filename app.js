eacdd622d0bde5af9a168e3544b92d759802b825
Soumyashis Karmakar <soumyashisk@gmail.com>
2025-11-17 19:07:40 +0530

    feat: implementng connection creating functionality with updated styles and module support

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const mapArea = document.getElementById("mapArea");
+const connectionsContainer = document.querySelector("#connections");
+
 let nodeId = 0;
+let connections = [];
 
 mapArea.addEventListener("dblclick", (e) => {
   const el = e.target;
@@ -8,7 +11,8 @@ mapArea.addEventListener("dblclick", (e) => {
   }
 });
 
-const calculatePosX = (x, node) =>
+// #region Nodes
+const calculateNodePosX = (x, node) =>
   Math.max(
     10,
     Math.min(
@@ -17,7 +21,7 @@ const calculatePosX = (x, node) =>
     )
   );
 
-const calculatePosY = (y, node) =>
+const calculateNodePosY = (y, node) =>
   Math.max(
     10,
     Math.min(
@@ -26,15 +30,15 @@ const calculatePosY = (y, node) =>
     )
   );
 
-function createNode(x, y) {
+export function createNode(x, y) {
   const node = document.createElement("div");
   node.className = "node";
   node.style.opacity = 0;
   node.style.pointerEvents = "none";
   mapArea.appendChild(node);
 
-  node.style.left = calculatePosX(x, node) + "px";
-  node.style.top = calculatePosY(y, node) + "px";
+  node.style.left = calculateNodePosX(x, node) + "px";
+  node.style.top = calculateNodePosY(y, node) + "px";
   node.dataset.id = nodeId;
   node.style.opacity = 1;
   node.style.pointerEvents = "all";
@@ -72,12 +76,6 @@ function createNode(x, y) {
     }
   };
 
-  makeDraggable(node);
-
-  return node;
-}
-
-function makeDraggable(node) {
   let isDragging = false;
 
   node.addEventListener("mousedown", (e) => {
@@ -86,12 +84,59 @@ function makeDraggable(node) {
 
   document.addEventListener("mousemove", (e) => {
     if (!isDragging) return;
-    node.style.left = calculatePosX(e.clientX, node) + "px";
-    node.style.top = calculatePosY(e.clientY, node) + "px";
-    // updateLines();
+    node.style.left = calculateNodePosX(e.clientX, node) + "px";
+    node.style.top = calculateNodePosY(e.clientY, node) + "px";
   });
 
   document.addEventListener("mouseup", () => {
     isDragging = false;
   });
+
+  return node;
 }
+
+//#endregion
+
+// #region Connections
+
+function createConnections() {
+  for (const el of connections) {
+    const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    for (const key in el) {
+      line.setAttribute(key, el[key]);
+    }
+    connectionsContainer.appendChild(line);
+  }
+}
+
+mapArea.addEventListener("click", (e) => {
+  const el = e.target;
+  if (el.id === "mapArea") {
+    createConnection(e);
+  }
+});
+
+let endClick = false;
+const connection = {
+  x1: 0,
+  y1: 0,
+  x2: 0,
+  y2: 0,
+};
+
+function createConnection(e) {
+  if (!endClick) {
+    connection.x1 = e.clientX;
+    connection.y1 = e.clientY;
+    console.log(connection, 1);
+  } else {
+    connection.x2 = e.clientX;
+    connection.y2 = e.clientY;
+    console.log(connection);
+    connections.push(connection, 2);
+    createConnections();
+  }
+
+  endClick = !endClick;
+}
+// #endregion
